f1e37f38c5da0a1197d5d40a1820396029ab75cf
ego008 <ego008@gmail.com>
2021-09-25 21:49:53 +0800

    json

---
+++ internal/json/json.go
@@ -0,0 +1,19 @@
+//go:build !jsoniter && !go_json
+// +build !jsoniter,!go_json
+
+package json
+
+import "encoding/json"
+
+var (
+	// Marshal is exported by gin/json package.
+	Marshal = json.Marshal
+	// Unmarshal is exported by gin/json package.
+	Unmarshal = json.Unmarshal
+	// MarshalIndent is exported by gin/json package.
+	MarshalIndent = json.MarshalIndent
+	// NewDecoder is exported by gin/json package.
+	NewDecoder = json.NewDecoder
+	// NewEncoder is exported by gin/json package.
+	NewEncoder = json.NewEncoder
+)
