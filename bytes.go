3cb1370e8dfc86e2dc00f8ebb5b03fbfb73588e5
Ybb <ego008@gmail.com>
2023-12-29 11:09:30 +0800

    Update bytes.go

--- bytes.go
+++ bytes.go
@@ -57,6 +57,20 @@ func Bconcat(slices [][]byte) []byte {
 	return tmp
 }
 
+// Bconcat2 concat a list of byte
+func Bconcat2(slices ...[]byte) []byte {
+	var totalLen int
+	for _, s := range slices {
+		totalLen += len(s)
+	}
+	tmp := make([]byte, totalLen)
+	var i int
+	for _, s := range slices {
+		i += copy(tmp[i:], s)
+	}
+	return tmp
+}
+
 func CloneBytes(src []byte) []byte {
 	dst := make([]byte, len(src))
 	copy(dst, src)
