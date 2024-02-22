223603b4f5d039987594a415813660caed41b003
God <ego008@gmail.com>
2024-02-22 14:22:47 +0800

    Update splock.go

--- splock/splock.go
+++ splock/splock.go
@@ -1,6 +1,9 @@
 package splock
 
+import "sync"
+
 // -- simpleLock
+var mutex = &sync.RWMutex{}
 
 type SL struct {
 	key  string
@@ -15,6 +18,8 @@ func (s *SimpleLock) Init(k string) *SL {
 	if s.mp == nil {
 		s.mp = map[string]*SL{}
 	}
+	mutex.Lock()
+	defer mutex.Unlock()
 	if v, ok := s.mp[k]; ok {
 		return v
 	}
@@ -24,14 +29,34 @@ func (s *SimpleLock) Init(k string) *SL {
 	return &sl
 }
 
+func (s *SimpleLock) HasLocked() (bool, string) {
+	if s.mp == nil {
+		return false, ""
+	}
+	mutex.RLock()
+	defer mutex.RUnlock()
+	for k := range s.mp {
+		if s.mp[k].lock {
+			return true, k
+		}
+	}
+	return false, ""
+}
+
 func (l *SL) IsLocked() bool {
+	mutex.RLock()
+	defer mutex.RUnlock()
 	return l.lock
 }
 
 func (l *SL) Lock() {
+	mutex.Lock()
 	l.lock = true
+	mutex.Unlock()
 }
 
 func (l *SL) UnLock() {
+	mutex.Lock()
 	l.lock = false
+	mutex.Unlock()
 }
