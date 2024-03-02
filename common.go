d7e01b77317981c9d232aeb04f30ba5c54e88c74
God <ego008@gmail.com>
2024-03-02 17:13:31 +0800

    Add formatInt

--- common.go
+++ common.go
@@ -1,5 +1,7 @@
 package goutils
 
+import "strconv"
+
 func SliceUniqInt(s []int) []int {
 	if len(s) == 0 {
 		return s
@@ -32,4 +34,33 @@ func SliceUniqStr(s []string) []string {
 		j++
 	}
 	return s[:j]
+}
+
+func FormatInt(number int64) string {
+	str := strconv.FormatInt(number, 10)
+	lStr := len(str)
+	digits := lStr
+	if number < 0 {
+		digits--
+	}
+	commas := (digits+2)/3 - 1
+	lBuf := lStr + commas
+	var sBuf [32]byte // pre allocate buffer at stack rather than make([]byte,n)
+	buf := sBuf[0:lBuf]
+	// copy str from the end
+	for si, bi, c3 := lStr-1, lBuf-1, 0; ; {
+		buf[bi] = str[si]
+		if si == 0 {
+			return string(buf)
+		}
+		si--
+		bi--
+		// insert comma every 3 chars
+		c3++
+		if c3 == 3 && (si > 0 || number > 0) {
+			buf[bi] = ','
+			bi--
+			c3 = 0
+		}
+	}
 }
